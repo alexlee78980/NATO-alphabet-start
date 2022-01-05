d1677bf6a01078d3714d214b932ac4b82b8a5d96
alexlee78980 <78987809+alexlee78980@users.noreply.github.com>
2022-01-05 12:47:50 -0800

    done

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,30 +1,42 @@
 student_dict = {
-    "student": ["Angela", "James", "Lily"], 
+    "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+# Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
+    # Access key and value
     pass
 
 import pandas
+
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
+# Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+    # Access index and row
+    # Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 nato_csv = pandas.read_csv("nato_phonetic_alphabet.csv")
-new_dict = {row.letter : row.code  for(index, row) in nato_csv.iterrows()}
-print(new_dict)
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-name = input("What is your name? :")
-list_nato = [new_dict.get(l.upper()) for l in name]
+new_dict = {row.letter: row.code for (index, row) in nato_csv.iterrows()}
+
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+def get_name():
+    try:
+        name = input("What is your name? :")
+        nato = [new_dict[l.upper()] for l in name]
+        return nato
+    except KeyError or TypeError:
+        print("Sorry, only letters in the alphabet please")
+        get_name()
+
+
+list_nato = get_name()
 print(list_nato)
